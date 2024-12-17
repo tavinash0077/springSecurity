89c0f0588470d2dcd0d70f23648c5fe54d2d0453
tavinash0077 <tavinash0077@gmail.com>
2024-12-17 11:50:11 +0530

    Added new feature in memory db

--- src/main/java/com/example/securitydemo/SpringSecurity.java
+++ src/main/java/com/example/securitydemo/SpringSecurity.java
@@ -1,15 +1,58 @@
 package com.example.securitydemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity {
 
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(withDefaults());
+        http.headers(headers ->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();//here we are returning http security obj
     }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}adminPass")
+                .roles("ADMIN")
+                .build();
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}userPass")
+                .roles("USER")
+                .build();
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(admin);
+
+        return userDetailsManager;
+    }
+
 }
